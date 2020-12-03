336e394b0d4ee3b080bac1c408f4cdd1f6b89f48
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-12-03 17:29:36 -0500

    done

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class MonkeyTypewriter {
     public static void main(String[] args) {
         String introduction = "It was the best of times,\n" +
@@ -20,19 +22,30 @@ public class MonkeyTypewriter {
                 "its noisiest authorities insisted on its being received, for good or for\n" +
                 "evil, in the superlative degree of comparison only.";
 
-        // Do all of the Monkey / Thread building here
-        // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
-        // A Tale Of Two Cities.
+        UnsafeCopier unsafeCopier = new UnsafeCopier(introduction);
+        SafeCopier safeCopier = new SafeCopier(introduction);
 
+        Thread[] unsafeMonkeys = new Thread[5];
+        for (int i=0; i<5; i++){
+            unsafeMonkeys[i] = new Thread(unsafeCopier);
+            unsafeMonkeys[i].start();
+        }
+
+        Thread[] safeMonkeys = new Thread[5];
+        for (int i=0; i<5; i++){
+            safeMonkeys[i] = new Thread(safeCopier);
+            safeMonkeys[i].start();
+        }
 
         // This wait is here because main is still a thread and we want the main method to print the finished copies
         // after enough time has passed.
         try {
             Thread.sleep(1000);
+            //in youtube he printed here
         } catch(InterruptedException e) {
             System.out.println("MAIN INTERRUPTED");
         }
-
-        // Print out the copied versions here.
+        System.out.println("Unsafe copy:\n" + unsafeCopier.copied + "\n");
+        System.out.println("Safe copy:\n" + safeCopier.copied + "\n");
     }
 }
